e0feeb7e23d96dfba9c2f744730104731fac6049
Giulia Alves Massoni <59745030+massonigiulia@users.noreply.github.com>
2023-04-15 18:52:34 -0300

    Update entity.ts

--- src/interfaces/entity.ts
+++ src/interfaces/entity.ts
@@ -1,1 +1,12 @@
+export interface IUsuario {
+    nome:string;
+    login:string;
+    email: string;
+    senha: string;
+    codSetor: number;
+    codUsuario: number; 
+}
 
+export interface IUsuarios {
+    usuarios: IUsuario[]
+}
